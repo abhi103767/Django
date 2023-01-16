d2e17638e53660a1784fb4e9a6e771f0b99a01be
abhi103767 <avinash1234sharma1@gmail.com>
2023-01-16 06:41:36 +0530

    comoleted django

--- project1/calc/serilizer.py
+++ project1/calc/serilizer.py
@@ -3,8 +3,8 @@ from rest_framework import routers, serializers, viewsets
 
 
 
-class DesinationSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = [ 'name', 'offer', 'image','price']
+# class DesinationSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = [ 'name', 'offer', 'image','price']
     
